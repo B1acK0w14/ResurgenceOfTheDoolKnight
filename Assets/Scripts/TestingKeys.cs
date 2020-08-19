d442bbe9cb9e83f51110583c5212db5767374813
David Penagos <davisto83pena@gmail.com>
2020-08-18 22:32:26 -0500

    Add weapons and bullets sprites. Add enemy sprite and action of this enemy.

---
+++ Assets/Scripts/TestingKeys.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingKeys : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 1. Mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Button Pressed");
+        }
+
+        // 2. Getting keyboard button events
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Using Jump. You can use this to make the player jump");
+        }
+
+        // 3. Getting axis for movement when user tap a keyboard (Left,Right,Up,Down) 
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (horizontal < 0f || horizontal > 0f)
+        {
+            Debug.Log("Horizontal axis is: " + horizontal);
+        }
+
+        if (vertical < 0f || vertical > 0f)
+        {
+            Debug.Log("Horizontal axis is: " + vertical);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public GameObject shooter;
+    private Transform _firePoint;
+
+    private void Awake()
+    {
+        _firePoint = transform.Find("FirePoint");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Call Start!");
+        Shoot();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Shoot()
+    {
+        Debug.Log("Work Shoot!");
+        if (bulletPrefab != null && _firePoint != null && shooter != null)
+        {
+            Debug.Log("Enter if!");
+            GameObject myBullet = Instantiate(bulletPrefab, _firePoint.position, Quaternion.identity) as GameObject;
+            Bullet bulletComponent = myBullet.GetComponent<Bullet>();
+
+            // If shooter is looking right or left
+            if (shooter.transform.localScale.x < 0f) {
+                // Left
+                bulletComponent.direction = Vector2.left;
+            } else {
+                // Right
+                bulletComponent.direction = Vector2.right;
+            }
+        }
+    }
+}
